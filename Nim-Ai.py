4d9879f5e5e91743eb04cf44f843097f3dbc1bf9
PiKitty518 <henrymccauleylynch@gmail.com>
2022-06-27 13:48:08 -0400

    Part 1

---
+++ Nim-Ai.py
@@ -0,0 +1,15 @@
+#Nim-AI
+#By PiKitty518
+
+print(" _   _ _____ __  __                     _____ ")
+print("| \ | |_   _|  \/  |              /\   |_   _|")
+print("|  \| | | | | \  / |  ______     /  \    | |  ")
+print("| . ` | | | | |\/| | |______|   / /\ \   | |  ")
+print("| |\  |_| |_| |  | |           / ____ \ _| |_ ")
+print("|_| \_|_____|_|  |_|          /_/    \_\_____|")
+print("         An AI that learns to play NIM        ")
+
+#Imports
+import random
+
+#Vars
